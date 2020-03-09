3f1a7db7ed5e2a35cb0530c6c4cb18f7e67271a5
pocwhh <654310862@qq.com>
2020-03-09 23:18:39 +0800

    init

---
+++ src/views/curriculum.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="curriculum">
+    <van-tabs v-model="active" animated swipeable>
+      <van-tab v-for="index in 4" :key="index" :title="'选项 ' + index">
+        内容 {{ index }}
+      </van-tab>
+    </van-tabs>
+  </div>
+</template>
+
+<script>
+import { Tab, Tabs } from 'vant'
+
+export default {
+  components: {
+    [Tab.name]: Tab,
+    [Tabs.name]: Tabs
+  },
+
+  data() {
+    return {
+      active: 0,
+    }
+  },
+
+  methods: {
+  }
+};
+</script>
+
+<style lang="less">
+.curriculum {
+  height: 100vh;
+}
+</style>
